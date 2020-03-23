48be2bdf8dd6eb53dd990755fb2c022fb6d0e6f6
unknown <gar.dem494@gmail.com>
2020-03-22 23:23:31 -0700

    Updates

---
+++ precache-manifest.0df892cd99e8cb96086ac6bae0bd7637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1068b740c6b6a9dcb0a05ca6b3df4aeb",
+    "url": "/youtubeApi/index.html"
+  },
+  {
+    "revision": "aac4a803d44c72789e16",
+    "url": "/youtubeApi/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a6d2a947af7f96b4b941",
+    "url": "/youtubeApi/static/css/main.d1bbbc60.chunk.css"
+  },
+  {
+    "revision": "aac4a803d44c72789e16",
+    "url": "/youtubeApi/static/js/2.4030a145.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/youtubeApi/static/js/2.4030a145.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6d2a947af7f96b4b941",
+    "url": "/youtubeApi/static/js/main.3cba1b38.chunk.js"
+  },
+  {
+    "revision": "e4414fd5bb0e08f905f1",
+    "url": "/youtubeApi/static/js/runtime-main.e217d81d.js"
+  }
+]);
